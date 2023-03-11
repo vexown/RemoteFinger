bb5f153cf11092fd460703c5cf86643bce6c4602
blankMCU <vexown@gmail.com>
2023-03-11 23:57:33 +0100

    I copied some stuff from FreeRTOS main_full demo and it worked lol - next to find out why, but for now comitting it

--- RaspberryPi_Projects/RaspberryPico/RemoteFinger/Application/Standard/main_blinky.c
+++ RaspberryPi_Projects/RaspberryPico/RemoteFinger/Application/Standard/main_blinky.c
@@ -26,48 +26,59 @@
  */
 
 /******************************************************************************
+ * NOTE 1:  This project provides two demo applications.  A simple blinky style
+ * project, and a more comprehensive test and demo application.  The
+ * mainCREATE_SIMPLE_BLINKY_DEMO_ONLY setting in main.c is used to select
+ * between the two.  See the notes on using mainCREATE_SIMPLE_BLINKY_DEMO_ONLY
+ * in main.c.  This file implements the comprehensive test and demo version.
+ *
  * NOTE 2:  This file only contains the source code that is specific to the
- * basic demo.  Generic functions, such FreeRTOS hook functions, and functions
- * required to configure the hardware are defined in main.c.
+ * full demo.  Generic functions, such FreeRTOS hook functions, and functions
+ * required to configure the hardware, are defined in main.c.
+ *
  ******************************************************************************
  *
- * main_blinky() creates one queue, and two tasks.  It then starts the
- * scheduler.
+ * main_full() creates all the demo application tasks and software timers, then
+ * starts the scheduler.  The web documentation provides more details of the
+ * standard demo application tasks, which provide no particular functionality,
+ * but do provide a good example of how to use the FreeRTOS API.
  *
- * The Queue Send Task:
- * The queue send task is implemented by the prvQueueSendTask() function in
- * this file.  prvQueueSendTask() sits in a loop that causes it to repeatedly
- * block for 200 milliseconds, before sending the value 100 to the queue that
- * was created within main_blinky().  Once the value is sent, the task loops
- * back around to block for another 200 milliseconds...and so on.
+ * In addition to the standard demo tasks, the following tasks and tests are
+ * defined and/or created within this file:
  *
- * The Queue Receive Task:
- * The queue receive task is implemented by the prvQueueReceiveTask() function
- * in this file.  prvQueueReceiveTask() sits in a loop where it repeatedly
- * blocks on attempts to read data from the queue that was created within
- * main_blinky().  When data is received, the task checks the value of the
- * data, and if the value equals the expected 100, toggles an LED.  The 'block
- * time' parameter passed to the queue receive function specifies that the
- * task should be held in the Blocked state indefinitely to wait for data to
- * be available on the queue.  The queue receive task will only leave the
- * Blocked state when the queue send task writes to the queue.  As the queue
- * send task writes to the queue every 200 milliseconds, the queue receive
- * task leaves the Blocked state every 200 milliseconds, and therefore toggles
- * the LED every 200 milliseconds.
+ * "Reg test" tasks - These fill both the core and floating point registers with
+ * known values, then check that each register maintains its expected value for
+ * the lifetime of the task.  Each task uses a different set of values.  The reg
+ * test tasks execute with a very low priority, so get preempted very
+ * frequently.  A register containing an unexpected value is indicative of an
+ * error in the context switching mechanism.
+ *
+ * "Check" task - The check task period is initially set to three seconds.  The
+ * task checks that all the standard demo tasks, and the register check tasks,
+ * are not only still executing, but are executing without reporting any errors.
+ * If the check task discovers that a task has either stalled, or reported an
+ * error, then it changes its own execution period from the initial three
+ * seconds, to just 200ms.  The check task also toggles an LED each time it is
+ * called.  This provides a visual indication of the system status:  If the LED
+ * toggles every three seconds, then no issues have been discovered.  If the LED
+ * toggles every 200ms, then an issue has been discovered with at least one
+ * task.
  */
+
+/* Standard includes. */
+#include <stdio.h>
 
 /* Kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
 
-/* Library includes. */
-#include <stdio.h>
-#include "hardware/gpio.h"
-#include <string.h>
+/* SDK includes */
 #include "pico/stdlib.h"
+#include <string.h>
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
+#include "hardware/gpio.h"
 
 /* Priorities at which the tasks are created. */
 #define mainQUEUE_RECEIVE_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
@@ -88,9 +99,12 @@ the queue empty. */
 /*-----------------------------------------------------------*/
 
 /*
- * Called by main in main.c.
+ * Called by main() to run the full demo (as opposed to the blinky demo) when
+ * mainCREATE_SIMPLE_BLINKY_DEMO_ONLY is set to 0.
  */
 void main_blinky( void );
+
+/*-----------------------------------------------------------*/
 
 /*
  * The tasks as described in the comments at the top of this file.
@@ -112,7 +126,7 @@ static int addr = 0x68;
 static void mpu6050_reset() {
     // Two byte reset. First byte register, second byte data
     // There are a load more options to set up the device in different ways that could be added here
-    uint8_t buf[] = {0x6B, 0x80};
+    uint8_t buf[] = {0x6B, 0x00};
     i2c_write_blocking(i2c_default, addr, buf, 2, false);
 }
 
@@ -154,7 +168,42 @@ static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
 
 void main_blinky( void )
 {
-    printf(" Starting main_blinky.\n");
+
+	stdio_init_all();
+#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+    #warning i2c/mpu6050_i2c example requires a board with I2C pins
+    puts("Default I2C pins were not defined");
+#else
+    printf("Hello, MPU6050! Reading raw data from registers...\n");
+
+    // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
+    i2c_init(i2c_default, 400 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+
+    mpu6050_reset();
+
+    int16_t acceleration[3], gyro[3], temp;
+
+    while (1) {
+        mpu6050_read_raw(acceleration, gyro, &temp);
+
+        // These are the raw numbers from the chip, so will need tweaking to be really useful.
+        // See the datasheet for more information
+        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
+        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
+        // Temperature is simple so use the datasheet calculation to get deg C.
+        // Note this is chip temperature.
+        printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+
+        sleep_ms(100);
+    }
+
+#endif
 
     /* Create the queue. */
 	xQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof( uint32_t ) );
@@ -200,9 +249,8 @@ const unsigned long ulValueToSend = 100UL;
 	for( ;; )
 	{
 		/* Place this task in the blocked state until it is time to run again. */
-		uint32_t SendToQueue_freq_ms = mainQUEUE_SEND_FREQUENCY_MS/10;
-		vTaskDelayUntil( &xNextWakeTime, SendToQueue_freq_ms );
-		printf("SendToQueue_freq_ms = %u ms\n", SendToQueue_freq_ms);
+		vTaskDelayUntil( &xNextWakeTime, mainQUEUE_SEND_FREQUENCY_MS );
+
 		/* Send to the queue - causing the queue receive task to unblock and
 		toggle the LED.  0 is used as the block time so the sending operation
 		will not block - it shouldn't need to block as the queue should always
@@ -231,44 +279,15 @@ const unsigned long ulExpectedValue = 100UL;
 		is it the expected value?  If it is, toggle the LED. */
 		if( ulReceivedValue == ulExpectedValue )
 		{
-			printf("LED STATE CHANGED \n");
 			gpio_xor_mask( 1u << mainTASK_LED );
-
-			/* Ok I have found that the MPU6050 example doesnt seem to work in the blinky RTOS demo but does in comprehensive RTOS demo - lets find out why*/
-			/* Instead of just blinking an LED, read some I2C stuff from the gyro/accel sensor */
-			#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
-			#warning i2c/mpu6050_i2c example requires a board with I2C pins
-			puts("Default I2C pins were not defined");
-			#else
-			printf("Hello, MPU6050! Reading raw data from registers...\n");
-
-			// This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
-			i2c_init(i2c_default, 400 * 1000);
-			gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-			gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-			gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-			gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-			// Make the I2C pins available to picotool
-			bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
-
-			mpu6050_reset();
-
-			int16_t acceleration[3], gyro[3], temp;
-
-			mpu6050_read_raw(acceleration, gyro, &temp);
-
-			// These are the raw numbers from the chip, so will need tweaking to be really useful.
-			// See the datasheet for more information
-			printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
-			printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-			// Temperature is simple so use the datasheet calculation to get deg C.
-			// Note this is chip temperature.
-			printf("Temp. = %f\n", (temp / 340.0) + 36.53);
-		#endif
-
 			ulReceivedValue = 0U;
 		}
 	}
 }
 /*-----------------------------------------------------------*/
+
+
+
+
+
 
