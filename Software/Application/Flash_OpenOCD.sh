7d8dc2afe52169c5b139b11f39e8234988a2348a
blankMCU <vexown@gmail.com>
2024-03-09 15:51:01 +0100

    Update path to elf in the Flashing script

--- Software/Application/Flash_OpenOCD.sh
+++ Software/Application/Flash_OpenOCD.sh
@@ -2,7 +2,7 @@
 
 echo "Flashing the board using Picoprobe and OpenOCD"
 
-openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program build/Standard/RemoteFinger_main.elf verify reset exit" 
+openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program build/SwComponents/RemoteFinger_main.elf verify reset exit" 
 
 echo "Press any key to exit..."
 read -n 1 -s 
